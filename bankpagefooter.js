88bd0150da5c1ac9a1a208c2bda91e1df20f88dc
Hrishika Dhakad <126436830+Hrishikaa1@users.noreply.github.com>
2025-10-15 12:14:58 +0530

    Add files via upload

---
+++ bankpagefooter.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import googlePlay from '@salesforce/resourceUrl/googlePlay';
+import phoneIcon from '@salesforce/resourceUrl/phoneIcon';
+import apple from '@salesforce/resourceUrl/apple';
+
+export default class Bankpagefooter extends LightningElement {
+    googlePlay = googlePlay;
+    phoneIcon = phoneIcon;
+    apple = apple;
+}
