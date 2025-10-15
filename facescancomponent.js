fb01df254499e52eaf90158d754fb7543a909bb7
Hrishika Dhakad <126436830+Hrishikaa1@users.noreply.github.com>
2025-10-15 12:14:22 +0530

    Add files via upload

---
+++ facescancomponent.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+import faceImage from '@salesforce/resourceUrl/faceImage';
+import number1Image from '@salesforce/resourceUrl/one';
+import number2Image from '@salesforce/resourceUrl/two';
+import number3Image from '@salesforce/resourceUrl/three';
+import number4Image from '@salesforce/resourceUrl/four';
+
+export default class Facescancomponent extends LightningElement {
+      faceImage = faceImage;
+    number1Image = number1Image;
+    number2Image = number2Image;
+    number3Image = number3Image;
+    number4Image = number4Image;
+}
