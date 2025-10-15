a3bd3696b35f425a3c9673e76822b6d5e20bcc4d
Hrishika Dhakad <126436830+Hrishikaa1@users.noreply.github.com>
2025-10-15 12:13:43 +0530

    Add files via upload

---
+++ headerComponent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import FlagHeader from '@salesforce/resourceUrl/Flag_header';
+
+export default class headerComponent extends LightningElement {
+
+   logoMarkup = `<img src="${FlagHeader}" alt="Bank Logo" class="logo-img"/>`;
+    // Handles Exit button click
+  
+}
