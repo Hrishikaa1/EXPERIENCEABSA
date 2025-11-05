9b426d6d9523c7f1c0645f6690891b388a9e5274
Hrishika Dhakad <hrishikadhakad1@gmail.com>
2025-11-05 10:45:02 +0530

    cjanges in js html yet to be done

--- force-app/main/default/lwc/practiced1/practiced1.js
+++ force-app/main/default/lwc/practiced1/practiced1.js
@@ -1,54 +1,56 @@
 import { LightningElement } from 'lwc';
 
 export default class Practiced1 extends LightningElement {
-    let shouldBeResolve = true ; 
+    shouldBeResolve = true;
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-      if (shouldBeResolve) {
-          resolve('success!');
-      }
+    createPromise() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (this.shouldBeResolve) {
+                    resolve('success!');
+                } else {
+                    reject('failed!');
+                }
+            }, 1000);
+        });
+    }
 
-      reject('failed!');
-  }, 1000);
-});
+    thenCatchApproach() {
+        this.createPromise()
+            .then((result) => {
+                console.log(`thenCatchApproach result => ${result}.`);
+            })
+            .catch((error) => {
+                console.log(`thenCatchApproach error => ${error}.`);
+            })
+            .finally(() => {
+                console.log('thenCatchApproach done.');
+            });
+    }
 
-// then/catch
-const thenCatchApproach = () => {
-  promise
-    .then((result) => {
-      console.log(`thenCatchApproach result => ${result}.`);
-    })
-    .catch((error) => {
-      console.log(`thenCatchApproach error => ${error}.`);
-    })
-    .finally(() => {
-      console.log('thenCatchApproach done.');
-    })
-}
+    async asyncAwaitApproach() {
+        try {
+            const result = await this.createPromise();
+            console.log(`asyncAwaitApproach result => ${result}.`);
+        } catch (error) {
+            console.error(error);
+            console.log(`asyncAwaitApproach error => ${error}.`);
+        } finally {
+            console.log('asyncAwaitApproach done.');
+        }
+    }
 
-// async/await
-const asyncAwaitApproach = async () => {
-  try {
-    const result = await promise;
-    console.log(`asyncAwaitApproach result => ${result}.`);
-  } catch (error) {
-    console.error(error);
-    console.log(`asyncAwaitApproach error => ${error}.`);
-  } finally {
-    console.log('asyncAwaitApproach done.');
-  }
-}
+    connectedCallback() {
+        // success
+        this.shouldBeResolve = true;
 
-// success
-shouldBeResolve = true;
+        this.thenCatchApproach();
+        this.asyncAwaitApproach();
 
-thenCatchApproach();
-asyncAwaitApproach();
+        // error
+        // this.shouldBeResolve = false;
 
-// error
-// shouldBeResolve = false;
-
-// thenCatchApproach();
-// asyncAwaitApproach();
+        // this.thenCatchApproach();
+        // this.asyncAwaitApproach();
+    }
 }
